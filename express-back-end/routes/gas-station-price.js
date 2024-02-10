7ac30a9d984b4763033bae56d2815031e210952d
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-02-09 20:21:47 -0600

    update

--- express-back-end/routes/gas-station-price.js
+++ express-back-end/routes/gas-station-price.js
@@ -1,14 +1,10 @@
-// gas-station-price.js
-
 const router = require("express").Router();
 const pool = require("../db");
 
 // Get all gas station prices
 router.get("/", async (req, res) => {
   try {
-    const gasStationPrices = await pool.query(
-      "SELECT * FROM gas_station_price"
-    );
+    const gasStationPrices = await pool.query("SELECT * FROM gas_station_price");
     res.json(gasStationPrices.rows);
   } catch (err) {
     console.error(err.message);
@@ -20,10 +16,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const gasStationPrice = await pool.query(
-      "SELECT * FROM gas_station_price WHERE gas_station_price_id = $1",
-      [id]
-    );
+    const gasStationPrice = await pool.query("SELECT * FROM gas_station_price WHERE gas_station_price_id = $1", [id]);
     res.json(gasStationPrice.rows[0]);
   } catch (err) {
     console.error(err.message);
@@ -32,13 +25,10 @@ router.get("/:id", async (req, res) => {
 });
 
 // Add a gas station price
-router.post("/", authorization, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { gas_station_id, gas_type_id, price, date } = req.body;
-    const newGasStationPrice = await pool.query(
-      "INSERT INTO gas_station_price (gas_station_id, gas_type_id, price, date) VALUES ($1, $2, $3, $4) RETURNING *",
-      [gas_station_id, gas_type_id, price, date]
-    );
+    const newGasStationPrice = await pool.query("INSERT INTO gas_station_price (gas_station_id, gas_type_id, price, date) VALUES ($1, $2, $3, $4) RETURNING *", [gas_station_id, gas_type_id, price, date]);
     res.json(newGasStationPrice.rows[0]);
   } catch (err) {
     console.error(err.message);
@@ -47,14 +37,11 @@ router.post("/", authorization, async (req, res) => {
 });
 
 // Update a gas station price
-router.put("/:id", authorization, async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { gas_station_id, gas_type_id, price, date } = req.body;
-    const updateGasStationPrice = await pool.query(
-      "UPDATE gas_station_price SET gas_station_id = $1, gas_type_id = $2, price = $3, date = $4 WHERE gas_station_price_id = $5",
-      [gas_station_id, gas_type_id, price, date, id]
-    );
+    const updateGasStationPrice = await pool.query("UPDATE gas_station_price SET gas_station_id = $1, gas_type_id = $2, price = $3, date = $4 WHERE gas_station_price_id = $5", [gas_station_id, gas_type_id, price, date, id]);
     res.json("Gas station price was updated!");
   } catch (err) {
     console.error(err.message);
@@ -63,13 +50,10 @@ router.put("/:id", authorization, async (req, res) => {
 });
 
 // Delete a gas station price
-router.delete("/:id", authorization, async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteGasStationPrice = await pool.query(
-      "DELETE FROM gas_station_price WHERE gas_station_price_id = $1",
-      [id]
-    );
+    const deleteGasStationPrice = await pool.query("DELETE FROM gas_station_price WHERE gas_station_price_id = $1", [id]);
     res.json("Gas station price was deleted!");
   } catch (err) {
     console.error(err.message);
