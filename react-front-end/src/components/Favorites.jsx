4a1a13fd025a2e9f438c00ec2582cdb3b43c881e
Sophia <chiaki.katsuno@live.ca>
2024-02-13 15:56:53 -0800

    change title of favorited gas stations

--- react-front-end/src/components/Favorites.jsx
+++ react-front-end/src/components/Favorites.jsx
@@ -7,49 +7,51 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
-    const [favorites, setFavorites] = useState([]);
-    const navigate = useNavigate();
-    
-    // useEffect(() => {
-    //     const getFavorites = async () => {
-    //     try {
-    //         const res = await axios.get("/favorites", {
-    //         headers: { token: localStorage.token },
-    //         });
-    //         console.log("Hello", res.data);
-    //         setFavorites(res.data);
-    //     } catch (err) {
-    //         console.error(err.message);
-    //     }
-    //     };
-    //     getFavorites();
-    // }, []);
-    
-    const handleDelete = async (id) => {
-        try {
-        await axios.delete(`/favorites/${id}`, {
-            headers: { token: localStorage.token },
-        });
-        setFavorites(favorites.filter((favorite) => favorite.gas_station_id !== id));
-        } catch (err) {
-        console.error(err.message);
-        }
-    };
-    
-    return (
-        <div>
-        <h1>Favorites</h1>
-        <div>
-            {favorites.map((favorite) => (
-            <GasStation
-                key={favorite.gas_station_id}
-                gasStation={favorite}
-                handleDelete={handleDelete}
-            />
-            ))}
-        </div>
-        </div>
-    );
+  const [favorites, setFavorites] = useState([]);
+  const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const getFavorites = async () => {
+//       try {
+//         const res = await axios.get("/favorites", {
+//           headers: { token: localStorage.token },
+//         });
+//         console.log("Hello", res.data);
+//         setFavorites(res.data);
+//       } catch (err) {
+//         console.error(err.message);
+//       }
+//     };
+//     getFavorites();
+//   }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`/favorites/${id}`, {
+        headers: { token: localStorage.token },
+      });
+      setFavorites(
+        favorites.filter((favorite) => favorite.gas_station_id !== id)
+      );
+    } catch (err) {
+      console.error(err.message);
     }
+  };
+
+  return (
+    <div className="favorites">
+      <h1>Favorited Gas Stations</h1>
+      <div>
+        {favorites.map((favorite) => (
+          <GasStation
+            key={favorite.gas_station_id}
+            gasStation={favorite}
+            handleDelete={handleDelete}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Favorites;
