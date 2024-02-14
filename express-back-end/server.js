68b6f6abd185751b70fca51cabec443e8845db5b
Sophia <chiaki.katsuno@live.ca>
2024-02-14 15:14:46 -0800

    cleaned master to make it runnable

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const app = express();
 
 // Routes
 const gasStationRoute = require("./routes/gas-stations");
